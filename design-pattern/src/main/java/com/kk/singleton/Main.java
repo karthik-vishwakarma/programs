c56e457b579f0230396be7c0851d0ad9e12aa556
kamlesh.vishwakarma <kamlesh.vishwakarma@rakuten.com>
2022-08-25 22:10:17 +0530

    EagerSingleton

---
+++ design-pattern/src/main/java/com/kk/singleton/Main.java
@@ -0,0 +1,10 @@
+package com.kk.singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        EagerSingleton eagerSingleton1 = EagerSingleton.getInstance();
+        System.out.println(eagerSingleton1);
+        EagerSingleton eagerSingleton2 = EagerSingleton.getInstance();
+        System.out.println(eagerSingleton2);
+    }
+}
