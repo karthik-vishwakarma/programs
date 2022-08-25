9e9b13a9f96d1fe1925127aaa37fc77681df9420
kamlesh.vishwakarma <kamlesh.vishwakarma@rakuten.com>
2022-08-25 22:59:23 +0530

    K

---
+++ design-pattern/src/main/java/com/kk/factory/FactoryPatternExample.java
@@ -0,0 +1,54 @@
+package com.kk.factory;
+
+abstract class Vehicle {
+    public abstract int getWheel();
+
+    public static Vehicle getInstance(String type, int wheel) {
+        if ("Bike".equals(type)) {
+            return new Car(wheel);
+        } else if ("Car".equals(type)) {
+            return new Bike(wheel);
+        }
+        return null;
+    }
+
+    public String toString() {
+        return "Wheel :: " + this.getWheel();
+    }
+}
+
+class Bike extends Vehicle {
+    private int wheel;
+
+    public Bike(int wheel) {
+        this.wheel = wheel;
+    }
+
+    @Override
+    public int getWheel() {
+        return wheel;
+    }
+}
+
+class Car extends Vehicle {
+
+    private int wheel;
+
+    public Car(int wheel) {
+        this.wheel = wheel;
+    }
+
+    @Override
+    public int getWheel() {
+        return wheel;
+    }
+}
+
+public class FactoryPatternExample {
+    public static void main(String[] args) {
+        Vehicle bike = Vehicle.getInstance("Bike", 2);
+        System.out.println(bike);
+        Vehicle car = Vehicle.getInstance("Car", 4);
+        System.out.println(car);
+    }
+}
